aed4e12d5ebd3b158810c5f40689815f51c1c598
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-24 13:42:55 +0100

    make comparison function

---
+++ src-tauri/src/python.rs
@@ -0,0 +1,35 @@
+use core::str;
+use semver::{Version, VersionReq};
+
+pub const MINIMUM_PYTHON: &str = ">=3.10";
+pub const PYTHON_SUFFIXES: [&str; 2] = ["", "3"];
+
+pub fn check_python_version(python_version: &str) -> bool {
+    println!("{python_version}");
+    let minimum_py = VersionReq::parse(MINIMUM_PYTHON).unwrap();
+    if let Ok(actual_version) = Version::parse(python_version.trim()) {
+        let result = minimum_py.matches(&actual_version);
+        return minimum_py.matches(&actual_version);
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::check_python_version;
+
+    #[test]
+    fn test_check_python_version() {
+        // Arrange
+        let good_version = "3.11.9";
+        let bad_version = "2.7.13";
+
+        // Act
+        let is_good = check_python_version(good_version);
+        let is_bad = !check_python_version(bad_version);
+
+        // Assert
+        assert!(is_good);
+        assert!(is_bad);
+    }
+}
