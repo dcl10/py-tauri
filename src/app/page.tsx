34e2547cd1d22467132c9fb51e5160dfae872a1f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-24 15:45:49 +0100

    call the method from the frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import Image from "next/image";
+import {invoke} from "@tauri-apps/api";
 
 export default function Home() {
+  const callAdd = (a: number, b: number) => {
+    invoke<number>("python_add", {a, b})
+      .then(answer => console.log(answer))
+      .catch(error => console.error(error));
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -47,6 +55,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <button onClick={() => (callAdd(2, 2))}>Add 2 and 2</button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
