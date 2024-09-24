81abf5e93000b46ea33f6f0f30a4a0aaa9d63e54
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-24 13:53:49 +0100

    Feature/check py version (#1)
    * add pyo3
    
    * fix next export
    
    * cargo fmt
    
    * add semver pkg
    
    * make comparison function
    
    * check python version
    
    * remove unused variable declaration
    
    * tidy up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use core::str;
+use std::process::Command;
+
+mod python;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|_app| {
+            use crate::python::{check_python_version, PYTHON_SUFFIXES};
+            for py in PYTHON_SUFFIXES.into_iter() {
+                let exe = format!("python{}", py);
+                match Command::new(&exe).args(&["--version"]).output() {
+                    Ok(output) => {
+                        if let Ok(version) = str::from_utf8(&output.stdout) {
+                            let v = version
+                                .split(" ")
+                                .last()
+                                .expect("Couldn't get python version");
+                            if check_python_version(v) {
+                                println!("Got a good version of Python");
+                            } else {
+                                println!("Wrong or no python installed");
+                            }
+                        }
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
