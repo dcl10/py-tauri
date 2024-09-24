5f573f6cacfd30049994584400aa7dc87e3040e3
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-24 14:39:46 +0100

    python code

---
+++ src-tauri/python/mod.py
@@ -0,0 +1,11 @@
+def add(a: int | float, b: int | float) -> int | float:
+    """Add to numbers, `a` and `b`, togther.
+
+    Args:
+        a (int | float): `a`
+        b (int | float): `b`
+
+    Returns:
+        int | float: `a + b`
+    """
+    return a + b
