a7f167f8c1b40e1cc5bc2457d06c4da777b6a0ef
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-25 15:49:55 +0100

    function to create venv

--- src-tauri/src/python.rs
+++ src-tauri/src/python.rs
@@ -1,5 +1,7 @@
 use core::str;
+use std::{process::Command, str::FromStr};
 use semver::{Version, VersionReq};
+use tauri::AppHandle;
 
 pub const MINIMUM_PYTHON: &str = ">=3.10";
 pub const PYTHON_SUFFIXES: [&str; 2] = ["", "3"];
@@ -10,6 +12,32 @@ pub fn check_python_version(python_version: &str) -> bool {
         return minimum_py.matches(&actual_version);
     }
     false
+}
+
+pub fn create_venv(path: &str, exe: &str, app_handle: AppHandle) -> Result<String, String> {
+    let app_dir = app_handle.path_resolver().app_data_dir();
+    if app_dir.is_none() {
+        return Err("Could not locate app data directory".to_string());
+    }
+    let venv_dir_buf = app_dir.unwrap().join(path);
+    let venv_dir_str_opt = venv_dir_buf.to_str();
+    if venv_dir_str_opt.is_none() {
+        return Err("Invalid virtual environment path".to_string());
+    }
+    let venv_str = venv_dir_str_opt.unwrap();
+    let cmd = Command::new(exe)
+        .args(&["-m", "venv", venv_str])
+        .status();
+
+    match cmd {
+        Ok(_) => {
+            let python_path_buf = venv_dir_buf.join("bin/python");
+            let python_path_str_opt = python_path_buf.to_str();
+            let python = String::from_str(python_path_str_opt.unwrap());
+            Ok(python.unwrap())
+        },
+        Err(_) => Err("Could not create the python virtual environment".to_string())
+    }
 }
 
 #[cfg(test)]
