d236ba99851b99278e01eb6def6d868758672f3b
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-25 17:05:12 +0100

    python code as embedded resource

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,18 +2,25 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use core::str;
-use std::process::Command;
+use std::{fs, process::Command};
 
 use pyo3::prelude::*;
 use python::create_venv;
+use tauri::AppHandle;
 
 mod python;
 
 #[tauri::command]
-fn python_add(a: f32, b: f32) -> Result<f32, String> {
-    let py_mod = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/python/mod.py"));
+fn python_add(a: f32, b: f32, app_handle: AppHandle) -> Result<f32, String> {
+    let py_mod_buf = app_handle
+        .path_resolver()
+        .resolve_resource("python/mod.py")
+        .expect("Could not get python module");
+    let py_path = py_mod_buf.to_str().unwrap();
+    let py_mod = fs::read_to_string(py_path).expect("Couldn't read in python module");
+    // let py_mod = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/python/mod.py"));
     let result = Python::with_gil(|py| -> Result<f32, String> {
-        let module = PyModule::from_code_bound(py, py_mod, "mod", "mod").unwrap();
+        let module = PyModule::from_code_bound(py, &py_mod, "mod.py", "mod").unwrap();
         let add = module.getattr("add").unwrap();
         let results = add.call((a, b), None).unwrap();
         if let Ok(answer) = results.extract::<f32>() {
