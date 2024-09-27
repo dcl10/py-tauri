14022479b17587b00ceea6e43d166099c230d985
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-27 14:27:22 +0100

    enable embedded or dynamic linking of python

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
+    let python_path = env::var("CUSTOM_PYTHON")
+        .expect("Please specify CUSTOM_PYTHON to static Python lib directory");
+    println!("cargo:rustc-link-lib=static=python3.11");
+    println!("cargo:rustc-link-search=native={}", python_path);
     tauri_build::build()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use core::str;
-use std::{fs, process::Command};
+use std::fs;
 
 use pyo3::prelude::*;
-use python::create_venv;
 use tauri::AppHandle;
-
-mod python;
 
 #[tauri::command]
 fn python_add(a: f32, b: f32, app_handle: AppHandle) -> Result<f32, String> {
@@ -34,31 +30,6 @@ fn python_add(a: f32, b: f32, app_handle: AppHandle) -> Result<f32, String> {
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            use crate::python::{check_python_version, PYTHON_SUFFIXES};
-            for py in PYTHON_SUFFIXES.into_iter() {
-                let exe = format!("python{}", py);
-                match Command::new(&exe).args(&["--version"]).output() {
-                    Ok(output) => {
-                        if let Ok(version) = str::from_utf8(&output.stdout) {
-                            let v = version
-                                .split(" ")
-                                .last()
-                                .expect("Couldn't get python version");
-                            if check_python_version(v) {
-                                println!("Got a good version of Python");
-                                let _ = create_venv("venv", &exe, app.handle());
-                                break;
-                            } else {
-                                println!("Wrong or no python installed");
-                            }
-                        }
-                    }
-                    Err(e) => println!("{:?}", e),
-                }
-            }
-            Ok(())
-        })
         .invoke_handler(tauri::generate_handler![python_add])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
